70b5c8d2d650f7a0ae2240ce432b029cbac4b23c
Daniel Pablo rubio <danpablr48@gmail.com>
2025-04-04 13:18:13 -0600

    mostrar las imagenes

--- ocr.py
+++ ocr.py
@@ -7,7 +7,10 @@ def cargar_imagen(ruta):
     return cv2.imread(ruta)
 
 def convertir_a_grises(imagen):
-    return cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    cv2.namedWindow("Ventana autoescalable gris", cv2.WINDOW_NORMAL)
+    cv2.imshow("Imagen en escala de grises", gris)  # Mostrar imagen en grises
+    return gris
 
 def detectar_fondo(gris, umbral=127):
     """Detecta si el fondo es claro u oscuro con base en el brillo promedio"""
@@ -16,25 +19,29 @@ def detectar_fondo(gris, umbral=127):
 
 def aplicar_umbral_fijo(gris):
     _, imagen_umbral = cv2.threshold(gris, 127, 255, cv2.THRESH_BINARY)
+    cv2.imshow("Umbral fijo aplicado", imagen_umbral) 
     return imagen_umbral
 
 def aplicar_umbral_adaptativo(gris):
     # Invertimos si el fondo es oscuro para mejorar el contraste
     gris_invertido = cv2.bitwise_not(gris)
-    return cv2.adaptiveThreshold(gris_invertido, 255, 
-                                 cv2.ADAPTIVE_THRESH_MEAN_C, 
-                                 cv2.THRESH_BINARY, 11, 2
-                                 )
+    cv2.namedWindow("Ventana autoescalable gris invertido", cv2.WINDOW_NORMAL)
+    cv2.imshow("Gris invertido", gris_invertido,)
+    return cv2.adaptiveThreshold(gris_invertido, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    
+    
 
+#Estraccion del texto de la imagen
 def extraer_texto(imagen_procesada):
     return pytesseract.image_to_string(imagen_procesada, lang="spa", )
 
-def guardar_texto(texto, nombre_archivo="texto_extraido.txt"):
+#Funcion para guardar la imagen en el txt
+def guardar_texto(texto, nombre_archivo="texto_extraido.txt"): 
     with open(nombre_archivo, "w", encoding="utf-8") as archivo:
         archivo.write(texto)
 
 def main():
-    ruta_imagen = "imagenes-ocr/cap3.jpg"  # <- Cambiar por la tuya
+    ruta_imagen = "imagenes-ocr/cade.jpg" 
     imagen = cargar_imagen(ruta_imagen)
     gris = convertir_a_grises(imagen)
     
@@ -49,12 +56,11 @@ def main():
         print("Modo claro")
 
     texto = extraer_texto(imagen_procesada)
-    
-    
-    guardar_texto(texto)
+
     texto_minusculas=texto.lower()
     texto_en_una_linea = texto_minusculas.replace('\n', ' ').strip()
-    print(texto_en_una_linea)
-
+    print(texto_en_una_linea)    
+    guardar_texto(texto_en_una_linea) 
+    cv2.waitKey()
 if __name__ == "__main__":
     main()
