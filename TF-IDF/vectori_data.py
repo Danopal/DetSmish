bef48c6f3ce5db76cb8aac44ed78d78df50e4fd0
Danopal <danpablr48@gmail.com>
2025-04-07 20:27:07 -0600

    Vectorizacion del dataset y la obtencion del vectorizador

--- TF-IDF/vectori_data.py
+++ TF-IDF/vectori_data.py
@@ -28,7 +28,12 @@ def preprocesamiento(texto, remove_punct=True, remove_numbers=True, remove_stopw
     
     if remove_stopwords:
         stop_words = set(stopwords.words(language))
-        words = [w for w in words if w not in stop_words]
+        palabras = [w for w in palabras if w not in stop_words]
+    
+    # Lematización básica (puedes sustituir por un lematizador si lo deseas)
+    if lemmatize:
+        palabras = [w if w not in string.punctuation else "" for w in palabras]  # Esto es solo un ejemplo simple de lematización
+
     
     return ' '.join(palabras)
 
@@ -42,21 +47,46 @@ def carga_de_DS(ruta_DS):
 #Creacion de la matriz TF-IDF
 def creacion_matriz(dataframe,**params_tdidf):
     vectorizador = TfidfVectorizer(**params_tdidf)
-    X_tfidf = vectorizador.fit_transform(dataframe['texto_limpio'])
+    X_tfidf = vectorizador.fit_transform(dataframe['preprocesamiento'])
     return X_tfidf, vectorizador, vectorizador.get_feature_names_out()
 
 #Guardar matriz en cvs
-def guardar_tfidf(X_tfidf, nombres_col, etiquetas, ruta_guardado='train_tfidf.csv' ):
-    dataF_tfidf = pd.DataFrame.sparse.from_spmatrix(X_tfidf, columnas = nombres_col)
+def guardar_tfidf(X_tfidf,feature_names,etiquetas, ruta_guardado='train_tfidf.csv' ):
+    dataF_tfidf = pd.DataFrame.sparse.from_spmatrix(X_tfidf, columns = feature_names)
     dataF_tfidf['Etiqueta'] = etiquetas.values
-    dataF_tfidf.to_csv(ruta_guardado, index = False, compression='gzip')
-    print("Dataset guardado en",ruta_guardado)
+    dataF_tfidf.to_csv(ruta_guardado, index = False) #     dataF_tfidf.to_csv(ruta_guardado, index = False,
+    print(f"Dataset guardado en {ruta_guardado} " )
 
-def guardar_vetorizador(vectorizador, dataf_limpio, ruta ):
-    
+def guardar_vetorizador(vectorizador, dataf_limpio, ruta_vectorizador = 'vectorizador_tfidf.pkl', ruta_DS_limpio = 'DS_limpio.cvs'):
+    joblib.dump(vectorizador, ruta_vectorizador)
+    print(f"vectorizador guardado en {ruta_vectorizador}")
+    dataf_limpio.to_csv(ruta_DS_limpio, index= False)
+    print(f"Datos guardados en {ruta_DS_limpio}")
+
 
 def main():
-    ruta_DS = 'Datasets/train.csv'
-    ruta_vectorizador = 'vectorizador.pkl'
-    ruta_DS_limpio = 'dataset_limpio.csv'
-    ruta_DStfidf = 'tfidf.csv'
+    RUTA_DS = 'Datasets/train.csv'
+    RUTA_VECTORIZADOR = 'vectorizador.pkl'
+    RUTA_DS_LIMPIO = 'dataset_limpio.csv'
+    RUTA_DS_TFIDF = 'tfidf.csv'
+
+    print("cargando los datos")
+    dataframe = carga_de_DS(RUTA_DS)
+
+    print("Limpiando el texto...")
+    dataframe['preprocesamiento'] = dataframe['Mensaje'].apply(lambda x: preprocesamiento(x,remove_punct=True, remove_numbers = True, remove_stopwords = True, lemmatize = True))
+
+    print("Vectorizando el DataSet")
+    X_tfidf, vectorizador, feature_names = creacion_matriz(dataframe, max_features=15000, ngram_range=(1, 2))
+
+    guardar_tfidf(X_tfidf ,feature_names, dataframe['Etiqueta'],RUTA_DS_TFIDF)
+
+    guardar_vetorizador(vectorizador, dataframe, RUTA_VECTORIZADOR, RUTA_DS_LIMPIO)
+
+    print("Proceso realizado correctamente")
+
+if __name__ == "__main__":
+    main()    
+
+
+
