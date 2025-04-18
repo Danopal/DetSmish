19aa5962fe164ff6a57590e9cf68b1f1df5f78a0
Danopal <danpablr48@gmail.com>
2025-04-17 23:39:09 -0600

    modifique svm

--- prediccion_con_svm.py
+++ prediccion_con_svm.py
@@ -1,5 +1,6 @@
 import joblib
 import numpy as np
+import pandas as pd
 from ocr import post_proc  # Asegúrate de tener esta función que procesará la imagen
 
 # Función para cargar el modelo SVM desde el archivo .pkl
@@ -8,8 +9,8 @@ def cargar_modelo(ruta_modelo):
     return modelo
 
 # Función para hacer predicciones con el modelo cargado
-def hacer_predicciones(modelo, datos_nuevos):
-    predicciones = modelo.predict(datos_nuevos)
+def hacer_predicciones(modelo, datos_nuevos_df):
+    predicciones = modelo.predict(datos_nuevos_df)
     
     predicciones_transformadas = ['no smishing' if pred == 'ham' else 'smishing' for pred in predicciones]
     
@@ -29,13 +30,19 @@ def preprocess_text(text):
     text = re.sub(r'\s+', ' ', text).strip()  # Eliminar espacios adicionales
     return text
 
-# Función para vectorizar el texto
+# Función para vectorizar el texto y convertirlo a DataFrame con nombres de características
 def vectorize_text(new_text, joblib_file):
     processed_text = preprocess_text(new_text)
     tfidf_vectorizer = load_tfidf_vectorizer(joblib_file)
+    
+    # Obtener la representación TF-IDF del texto
     text_vector = tfidf_vectorizer.transform([processed_text])
-    text_vector_dense = text_vector.toarray()
-    return text_vector_dense
+    
+    # Convertir el resultado en un DataFrame con los nombres de características
+    feature_names = tfidf_vectorizer.get_feature_names_out()
+    text_vector_df = pd.DataFrame(text_vector.toarray(), columns=feature_names)
+    
+    return text_vector_df
 
 # Ejemplo de uso
 if __name__ == "__main__":
@@ -47,15 +54,13 @@ if __name__ == "__main__":
     new_text = post_proc("imagenes-ocr/cap1.jpg")  
     
     # Vectorizar el texto
-    vectorized_result = vectorize_text(new_text, tfidf_joblib_file)
-    # Convertir el vector TF-IDF disperso a denso
-    vectorized_result = vectorized_result.toarray()
+    vectorized_result_df = vectorize_text(new_text, tfidf_joblib_file)
 
     # Cargar el modelo SVM
     modelo_svm = cargar_modelo(svm_model_file)
     
     # Hacer predicciones
-    predicciones = hacer_predicciones(modelo_svm, vectorized_result)
+    predicciones = hacer_predicciones(modelo_svm, vectorized_result_df)
     
     # Mostrar la predicción
-    print(f"Predicción del mensaje {new_text} es: {predicciones[0]}") 
+    print(f"Predicción del mensaje '{new_text}' es: {predicciones[0]}")
